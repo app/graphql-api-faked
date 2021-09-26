56f8f387b8a4e87dce64c8a5ed321781155e2264
Andrey Paskal <apaskal@gmail.com>
2021-09-26 16:14:40 +0300

    Update for reading stdin as well as params

--- run-gql.sh
+++ run-gql.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
-if [ ! -f $1 ] || [ $# -ne 1 ]; then
-  echo "Usage:"
-  echo "$0 your-query.gql"
-else
-  LINE="$(cat $1 | tr -d '\n')"
+run () {
   QUERY=$(jq -n --arg q "$LINE" '{ query: $q }')
-
   echo -e "Request\n" $QUERY
   echo Response
   curl -s -H "Content-Type: application/json; charset=utf-8" -d "$QUERY" http://localhost:3080/graphql | jq .
-fi
+}
 
+# Prepare data from input :)
+if read -t 0 ; then
+  echo "Waiting stdin..."
+  LINE=$(</dev/stdin)
+  run
+else
+  if [[ ! ${@} ]]; then
+    echo "Usage:"
+    echo "$0 your-query.gql"
+  else
+    if [ ! -f $1 ]; then
+      echo File "$1" not found!
+      echo "Usage:"
+      echo "$0 your-query.gql"
+    else
+      echo "stdin is empty"
+      LINE="$(cat $1 | tr -d '\n')"
+      run
+    fi
+  fi
+fi

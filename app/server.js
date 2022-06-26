46ba390f2d5abd9379b1f06623b6ec09cabba506
Andrey Paskal <apaskal@gmail.com>
2022-06-26 13:15:05 +0300

    Prettier code formatting

--- app/server.js
+++ app/server.js
@@ -1,66 +1,72 @@
-import express from 'express'
-import cors from 'cors'
-import jwt from 'jsonwebtoken'
-import cookie from 'cookie'
-import { graphqlHTTP } from 'express-graphql'
-import { mergeTypeDefs } from '@graphql-tools/merge'
-import { makeExecutableSchema } from '@graphql-tools/schema'
-import expressPlayground from 'graphql-playground-middleware-express'
-import auth from 'auth'
+import express from "express";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { graphqlHTTP } from "express-graphql";
+import { mergeTypeDefs } from "@graphql-tools/merge";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import expressPlayground from "graphql-playground-middleware-express";
+import auth from "auth";
 
-const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || '3080'
-const protocol = process.env.NODE_ENV === 'development' ? "http" : "https"
-const endpoint = process.env.NODE_ENV === 'development' ?
-  `${protocol}://127.0.0.1:${port}/graphql` : `${protocol}://signapi.art/graphql`
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || "3080";
+const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+const endpoint =
+  process.env.NODE_ENV === "development"
+    ? `${protocol}://127.0.0.1:${port}/graphql`
+    : `${protocol}://signapi.art/graphql`;
 
-const typeDefs = mergeTypeDefs([
-  auth.typeDefs,
-]);
+const typeDefs = mergeTypeDefs([auth.typeDefs]);
 
-const resolvers = [
-  auth.resolvers,
-];
+const resolvers = [auth.resolvers];
 
-const schema = makeExecutableSchema({ typeDefs, resolvers })
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const app = express()
-const whiteList = ['http://localhost','*']
+const app = express();
+const whiteList = ["http://localhost", "*"];
 const corsOptions = {
-  origin: (origin, callback) =>  callback(null, whiteList.includes('*') || whiteList.includes(origin)),
+  origin: (origin, callback) =>
+    callback(null, whiteList.includes("*") || whiteList.includes(origin)),
   credentials: true,
-}
-app.use( cors(corsOptions),)
+};
+app.use(cors(corsOptions));
 
-app.get('/', (req, res) => res.redirect(`https://www.signapi.art`));
+app.get("/", (req, res) => res.redirect(`https://www.signapi.art`));
 
 app.get(
-  '/playground',
+  "/playground",
   expressPlayground.default({
     // endpoint: `http://127.0.0.1:${port}/graphql`,
     endpoint,
-  }),
-)
+  })
+);
 
-app.use('/graphql', graphqlHTTP( async (req,res) => {
-  var token = req.header('Authorization')
-  var user
-  if (token) {
-    token = token.replace(/^.*\s+/, "")
-    try {
-      user = jwt.verify(token,auth.secret)
-    } catch (error) {
-      console.log(`Unable to verify token: ${token}`);
-      throw new Error(`Wrong or missing token`)
+app.use(
+  "/graphql",
+  graphqlHTTP(async (req, res) => {
+    var token = req.header("Authorization");
+    var user;
+    if (token) {
+      token = token.replace(/^.*\s+/, "");
+      try {
+        user = jwt.verify(token, auth.secret);
+      } catch (error) {
+        console.log(`Unable to verify token: ${token}`);
+        throw new Error(`Wrong or missing token`);
+      }
     }
-  }
-  const cookies = cookie.parse(req.headers.cookie || '');
-  const blacklist = cookies.blacklist ? JSON.parse(cookies.blacklist) : []
-  const context = { req, res, token, blacklist, user }
-  return {
-    schema: schema,
-    graphiql: true,
-    context,
-  }
-}))
-app.listen(port, host, () => console.log(`Node.js API server is listening on http://127.0.0.1:${port}/graphql`))
+    const cookies = cookie.parse(req.headers.cookie || "");
+    const blacklist = cookies.blacklist ? JSON.parse(cookies.blacklist) : [];
+    const context = { req, res, token, blacklist, user };
+    return {
+      schema: schema,
+      graphiql: true,
+      context,
+    };
+  })
+);
+app.listen(port, host, () =>
+  console.log(
+    `Node.js API server is listening on http://127.0.0.1:${port}/graphql`
+  )
+);

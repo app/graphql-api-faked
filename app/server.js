20ada5f9e515aca9008cd335692479af1f8bdc36
Andrey Paskal <apaskal@gmail.com>
2022-06-26 13:30:45 +0300

    This commit adds rest parametes syntax with array destruct for handler function

--- app/server.js
+++ app/server.js
@@ -31,7 +31,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => res.redirect(`https://www.signapi.art`));
+app.get("/", (...[, res]) => res.redirect(`https://www.signapi.art`));
 
 app.get(
   "/playground",
